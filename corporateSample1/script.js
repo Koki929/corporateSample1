95badc7c69a5fefd0b5dc95b4f0a4562a9c9f41d
koki0859 <2013edcckokimatsuhita@gmail.com>
2019-11-14 00:51:57 +0900

    編集11/14

--- corporateSample1/script.js
+++ corporateSample1/script.js
@@ -11,20 +11,45 @@ $(function() {
 
     // accordion
     $('.news-list-item').click(function() {
-        var $newscontent = $(this).find('.news-content');
-        if($newscontent.hasClass('open')) { 
-          $newscontent.removeClass('open');
-          $newscontent.slideUp();
-    
-          $(this).find('span').text('+');
-          
-        } else {
-          $newscontent.addClass('open'); 
-          $newscontent.slideDown();
-          
-          $(this).find('span').text('-');
-          
+      var $newscontent = $(this).find('.news-content');
+      if($newscontent.hasClass('open')) { 
+        $newscontent.removeClass('open');
+        $newscontent.slideUp();
+  
+        $(this).find('span').text('+');
+        
+      } else {
+        $newscontent.addClass('open'); 
+        $newscontent.slideDown();
+        
+        $(this).find('span').text('-');
+        
+      }
+    });
+
+    // scroll
+    $('.top-btn').click(function(){
+      $('html,body').animate({ 
+        'scrollTop': 0 
+      }, 500);
+    });
+
+    $('header, footer a').click(function(){
+      var id = $(this).attr('href');
+      var position = $(id).offset().top;
+      $('html,body').animate({ 
+        'scrollTop': position 
+      }, 500);
+    });
+
+    $(window).scroll(function (){
+      $('.fadein').each(function(){
+        var elemPos = $(this).offset().top,
+          scroll = $(window).scrollTop(),
+          windowHeight = $(window).height();
+        if (scroll > elemPos - windowHeight + 100){
+          $(this).addClass('scrollin');
         }
       });
-
+    });
 });
